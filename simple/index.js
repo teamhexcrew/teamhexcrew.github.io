b4df4e793c993040793364fb29012577803a7de6
kaviyarasu26 <101913405+kaviyarasu26@users.noreply.github.com>
2022-05-06 08:07:37 +0530

    almost complete

--- simple/index.js
+++ simple/index.js
@@ -1,4 +1,44 @@
-/*function aboutproject(){
+function aboutproject(){
     document.getElementById('main_ap').style.display="block";
-    document.getElementById('kingofking').style.color="red";
-}*/
+    document.getElementById('team12').style.display="none";
+    document.getElementById('homepage').style.display="none";
+    document.getElementById('thor26').style.display="none";
+    document.getElementById('makeaking').style.display="none";
+    document.getElementsByTagName('a').style.color="gray";
+}
+function homee12(){
+    document.getElementById('homepage').style.display="flex";
+    document.getElementById('team12').style.display="none";
+    document.getElementById('main_ap').style.display="none";
+    document.getElementById('thor26').style.display="none";
+    document.getElementById('makeaking').style.display="none";
+    document.getElementsByTagName('a').style.color="gray";
+
+}
+function gal12(){
+    document.getElementById('homepage').style.display="none";
+    document.getElementById('team12').style.display="none";
+    document.getElementById('main_ap').style.display="none";
+    document.getElementById('thor26').style.display="flex";
+    document.getElementById('makeaking').style.display="none";
+    document.getElementsByTagName('a').style.color="gray";
+
+}
+function teammem12(){
+    document.getElementById('homepage').style.display="none";
+    document.getElementById('team12').style.display="flex";
+    document.getElementById('main_ap').style.display="none";
+    document.getElementById('thor26').style.display="none";
+    document.getElementById('makeaking').style.display="none";
+    document.getElementsByTagName('a').style.color="gray";
+
+}
+function demopage12(){
+    document.getElementById('homepage').style.display="none";
+    document.getElementById('team12').style.display="none";
+    document.getElementById('main_ap').style.display="none";
+    document.getElementById('thor26').style.display="none";
+    document.getElementById('makeaking').style.display="block";
+    document.getElementsByTagName('a').style.color="gray";
+
+}
